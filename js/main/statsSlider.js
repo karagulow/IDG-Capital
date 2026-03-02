ce838247c6c2badb4315d7c65596c3a7ae1e7c0a
yzums <nagliyavarec@gmail.com>
2026-03-02 17:13:09 +0300

    fix: scroll bugs

--- js/main/statsSlider.js
+++ js/main/statsSlider.js
@@ -1,45 +1,45 @@
-const swiper = new Swiper('.stats__wrapper', {
-	slidesPerView: 1.3,
-	spaceBetween: 16,
-	speed: 600,
+const swiper = new Swiper(".stats__wrapper", {
+  slidesPerView: 1.3,
+  spaceBetween: 16,
+  speed: 600,
 
-	breakpoints: {
-		0: {
-			allowTouchMove: true,
-			slidesPerView: 1.3,
-		},
-		480: {
-			slidesPerView: 2,
-		},
-		1024: {
-			slidesPerView: 3,
-			allowTouchMove: false,
-		},
-	},
+  breakpoints: {
+    0: {
+      allowTouchMove: true,
+      slidesPerView: 1.3,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+      allowTouchMove: false,
+    },
+  },
 
-	navigation: {
-		nextEl: '.stats__arrow-btn_next',
-		prevEl: '.stats__arrow-btn_prev',
-		disabledClass: 'stats__arrow-btn_disabled',
-	},
+  navigation: {
+    nextEl: ".stats__arrow-btn_next",
+    prevEl: ".stats__arrow-btn_prev",
+    disabledClass: "stats__arrow-btn_disabled",
+  },
 });
 
 if (window.innerWidth > 1024) {
-	gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger);
 
-	const slidesCount = swiper.slides.length;
-	const scrollLength = slidesCount * window.innerHeight * 0.6;
+  const slidesCount = swiper.slides.length;
+  const scrollLength = slidesCount * window.innerHeight * 0.6;
 
-	ScrollTrigger.create({
-		trigger: '.stats',
-		start: 'center center',
-		end: `+=${scrollLength}`,
-		pin: true,
-		scrub: 0.1,
-		snap: 1 / (slidesCount - 1),
+  ScrollTrigger.create({
+    trigger: ".stats",
+    start: "center center",
+    end: `+=${scrollLength}`,
+    pin: true,
+    scrub: 0.1,
+    snap: 1 / (slidesCount - 1),
 
-		onUpdate(self) {
-			swiper.setProgress(self.progress);
-		},
-	});
+    onUpdate(self) {
+      swiper.setProgress(self.progress);
+    },
+  });
 }
